b4be83712a0ab2f4b85d6956727e47f4f25df789
tswbyaj <chueyangxaiblong1999@gmail.com>
2022-06-03 15:31:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+
+const username ="chueyang"
+const password = "hnPB111"
+const cluster = "cluster0"
+const dbname = "log-monitoring"
+
+
+app.use(express.json())
+app.use(express())
+
+mongoose.connect(
+    `mongodb+srv://${username}:${password}@cluster0.cw347.mongodb.net/${dbname}?retryWrites=true&w=majority`
+)
